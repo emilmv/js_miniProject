82c60d942323a7d08cfe8f1ae7669e8ecd65356b
emilmv <emilnm@code.edu.az>
2024-06-06 16:33:36 +0400

    navbar-partially-completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const buttons=document.querySelectorAll('.button');
+// buttons.forEach(function(button){
+//     button.addEventListener('click', function(ev){
+//         ev.preventDefault();
+//         button.style.color='#FFFFFF';
+//     })
+// })
